67235643d787ea0992fdf6cbee0a654455984929
Lakello <slavka779@mail.ru>
2026-06-10 18:08:37 +0700

    infra(terraform): add network module with VPC, subnets, NAT and security groups

--- infra/terraform/envs/dev/variables.tf
+++ infra/terraform/envs/dev/variables.tf
@@ -7,3 +7,28 @@ variable "folder_id" {
   description = "ID папки в Yandex.Cloud"
   type        = string
 }
+
+variable "environment" {
+  description = "Название окружения"
+  type        = string
+}
+
+variable "instance_tags" {
+  description = "Теги для экземпляров"
+  type        = map(string)
+}
+
+variable "availability_zones" {
+  description = "Зоны доступности"
+  type        = list(string)
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR блок для публичной подсети"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR блок для приватной подсети"
+  type        = string
+}
